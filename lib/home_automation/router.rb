fdb104098dda3b8167e6df3be5fdb5e76b21d207
Kieren Davies <kieren@kdavi.es>
2015-05-25 21:22:55 +0000

    Intercept router failures and raise meaningful errors

--- lib/home_automation/router.rb
+++ lib/home_automation/router.rb
@@ -3,12 +3,26 @@ require 'uri'
 
 module HomeAutomation
   module Router
+
+    class RouterError < StandardError
+    end
+
+    class DeviceLockedError < RouterError
+    end
+
+    class RequestError < RouterError
+    end
+
     def self.get_page path
       uri = URI.parse("http://www.routerlogin.com/#{path}")
       http = Net::HTTP.new(uri.host, uri.port)
       request = Net::HTTP::Get.new(uri.request_uri)
       request.basic_auth(Rails.application.secrets.router_username, Rails.application.secrets.router_password)
       response = http.request(request)
+
+      raise RequestError.new response.code unless response.code == '200'
+      raise DeviceLockedError if response.body =~ /is managing this device/
+
       response.body
     end
 
