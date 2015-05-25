2bb7eff65909c7f201b8394307b65d4c2c150bcb
Kieren Davies <kieren@kdavi.es>
2015-05-25 20:17:51 +0000

    Create Devise user model which auths with LDAP

--- Gemfile
+++ Gemfile
@@ -26,6 +26,9 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem "twitter-bootstrap-rails"
 
+gem 'devise'
+gem 'devise_ldap_authenticatable'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,8 @@
 require 'home_automation/router'
 
 class DashboardController < ApplicationController
+  before_filter :authenticate_user!
+
   def index
     @mac_addresses = HomeAutomation::Router.attached_mac_addresses
   end
