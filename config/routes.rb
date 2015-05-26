a3f0d400b7a1fccb5464b84393cfa5a1f4fb3e35
Kieren Davies <kieren@kdavi.es>
2015-05-26 16:48:28 +0000

    Create devices controller and list view

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'dashboard#index'
 
+  get 'devices' => 'devices#list'
+  get 'devices/:uid' => 'devices#list'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
