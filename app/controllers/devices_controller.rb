4e9ea7e130b25521270b1fea9a2ce94a238f54e6
Kieren Davies <kieren@kdavi.es>
2015-05-31 08:45:04 +0000

    Order devices by last seen

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -2,11 +2,11 @@ class DevicesController < ApplicationController
   before_filter :authenticate_user!
 
   def list_all
-    @devices = Device.all
+    @devices = Device.order(last_seen_at: :desc)
   end
 
   def list_for_user
-    @devices = User.find_by!(uid: params[:uid]).devices
+    @devices = User.find_by!(uid: params[:uid]).devices.order(last_seen_at: :desc)
   end
 
   def update
